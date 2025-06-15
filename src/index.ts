7f671110394846f26b514254906b0f4c7a163c90
tsaxking <taylorking206@u.boisestate.edu>
2025-06-15 00:13:53 -0600

    init webhook

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Redis } from './redis';
+import { config } from 'dotenv';
+import { z } from 'zod';
+config();
+
+const app = express();
+const server = createServer(app);
+const PORT = Number(process.env.PORT) || 3000;
+Redis.connect().then(res => {
+    if (res.isOk()) {
+        console.log('Redis connected successfully');
+    } else {
+        console.error('Failed to connect to Redis:', res.error);
+    }
+});
+
+const serverChannel = Redis.createChannel('server', {
+    ping: z.string(),
+}, {
+    ping: z.string(),
+});
+app.post('/', 
+express.json(),
+(req, res) => {
+    try {
+        serverChannel.send('ping', 'pong');
+    } catch (error) {
+        console.error('Error processing request:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
