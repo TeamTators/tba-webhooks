c95d314282f145ec2a4c50d863cfbca16dca1ec9
taylor <taylor.king206@u.boisestate.edu>
2025-09-13 00:35:57 -0600

    fix lib/esm problem

--- lib/esm/index.d.ts
+++
@@ -1,3 +0,0 @@
-import '@total-typescript/ts-reset';
-export declare const generateWebhookHmac: (payload: string, secret: string) => string;
-export declare const main: (PORT?: number, secret?: string, redisName?: string) => Promise<void>;
